fe544ec7795cee180cef910061f6ccdf987146db
kva-devops <kva.devops@gmail.com>
2021-01-12 12:18:37 +0000

    add yum update

---
+++ install_docker_centos.sh
@@ -0,0 +1,25 @@
+# update repo and install docker (Centos). After execute this file enter this command: sudo chmod u+x filename.sh
+#!/bin/bash
+sudo yum update -y
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce -y
+sudo yum install docker-ce-cli -y
+sudo yum install containerd.io
+
+sudo systemctl start docker
+
+sudo docker run hello-world
